9762bb6227c8553541bac0c38be79721f461e3dd
David Onuta <ogagaonuta@gmail.com>
2023-11-24 06:45:55 +0100

    Add and update files

---
+++ Python/Cisco-OpenEDG/Python_Essentials-1/triangles.py
@@ -0,0 +1,61 @@
+'''
+Triangles
+'''
+
+
+from operator import is_
+
+
+def is_a_triangle(a, b, c):
+	# The below two conditions work the same (NEGATION OF CONDITIONS)
+
+	# if ((a + b <= c) or (b + c <= a) or (c + a <= b)):
+	# 	return (False)	
+	# return (True)
+
+	return ((a + b > c) and (b + c > a) and (c + a > b))
+
+
+# print(is_a_triangle(1, 1, 1))
+# print(is_a_triangle(1, 1, 3))
+
+# a = float(input("Enter the first side's length: "))
+# b = float(input("Enter the second side's length: "))
+# c = float(input("Enter the third side's length: "))
+
+# if (is_a_triangle(a, b, c)):
+# 	print("Yes, it can be a triangle.")
+# else:
+# 	print("No, it can't be a triangle.")
+
+
+def is_a_right_triangle(a, b, c):
+	if (not is_a_triangle(a, b, c)):
+		return (False)
+
+	if ((c > a) and (c > b)):
+		return (c ** 2 == a ** 2 + b ** 2)
+	if ((b > a) and (b > c)):
+		return (b ** 2 == a ** 2 + c ** 2)
+	if ((a > b) and (a > c)):
+		return (a ** 2 == b ** 2 + c ** 2)
+	
+
+# print(is_a_right_triangle(5, 3, 4))
+# print(is_a_right_triangle(1, 3, 4))
+
+
+def heron(a, b, c):
+	p = (a + b + c) / 2
+
+	return ((p * (p - a) * (p - b) * (p - c)) ** 0.5)
+
+
+def area_of_triangle(a, b, c):
+	if (not is_a_triangle(a, b, c)):
+		return (None)
+
+	return (heron(a, b, c))
+
+
+print(area_of_triangle(1., 1., 2. ** .5))
